fc6b1d2a889629d2e529b80fe9aa5f1538d2cb03
Naresh Kumar <nareshkumar.pilani@gmail.com>
2021-05-28 09:07:06 +0530

    Angular Routing, Search by Category

--- src/main/java/com/training/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/training/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.training.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.training.ecommerce.domain.Product;
 
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	 Page<Product> findByProductCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	
 }
