c29acf1362c1ade10c8bfc4fac40e487bce8a321
Naresh Kumar <nareshkumar.pilani@gmail.com>
2021-05-28 12:19:38 +0530

    Dynamic Product Category : Expose Ids Spring Data Rest

--- src/main/java/com/training/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/training/ecommerce/config/DataRestConfig.java
@@ -1,5 +1,13 @@
-package com.training.ecommerceconfig;
+package com.training.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -12,6 +20,13 @@ import com.training.ecommerce.domain.ProductCategory;
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
 
+	private EntityManager entityManager;
+	
+	@Autowired
+	DataRestConfig(EntityManager entityManager){
+		this.entityManager = entityManager;
+	}
+	
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		
@@ -29,6 +44,23 @@ public class DataRestConfig implements RepositoryRestConfigurer {
 		 .withCollectionExposure((metdata,httpMethods) ->  httpMethods.disable(theUnsupportedActions))
 		 ;		
 		
+		//call an internal method
+		exposedIds(config);
+		
+	}
+
+	private void exposedIds(RepositoryRestConfiguration config) {
+		
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		
+		List<Class> entityClasses = new ArrayList<>();
+		
+		for(EntityType tempEntityType : entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+		}
+		
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 	
 }
